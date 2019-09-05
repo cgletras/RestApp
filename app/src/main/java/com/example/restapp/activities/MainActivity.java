f9069316e9fb368958283a0aaafda38acf342259
Carlos Gustavo de Lacerda Stein <cgletras@gmail.com>
2019-09-05 15:50:00 -0300

    Video aula 3 - Objetos criados no java/entities

--- app/src/main/java/com/example/restapp/activities/MainActivity.java
+++ app/src/main/java/com/example/restapp/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.restapp;
+package com.example.restapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.restapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
